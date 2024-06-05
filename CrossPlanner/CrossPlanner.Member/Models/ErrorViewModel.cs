4bc195e775555d3ffd50cfbae9fffaf57a551403
MartinMcGonigle <martin_mcgonigle@outlook.com>
2024-06-05 19:04:08 +0000

    Initial commit.

---
+++ CrossPlanner/CrossPlanner.Member/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrossPlanner.Member.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CrossPlanner/CrossPlanner.Staff/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrossPlanner.Staff.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
